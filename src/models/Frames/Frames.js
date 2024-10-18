702763a2c73043ce4b73efa0a9ff87dbcf31878d
himmatprabhkar <himmatprabhkar@gmail.com>
2024-10-18 11:25:49 +0530

    added new functinloity

--- src/models/Frames/Frames.js
+++ src/models/Frames/Frames.js
@@ -1,13 +1,23 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const frameSchema = new mongoose.Schema({
-  size: String,
-  type: String,
-  frame_url: String,
-  quantity: Number,
-  created_at: { type: Date, default: Date.now },
-  updated_at: { type: Date, default: Date.now }
+  frameName: { type: String, required: true }, // Frame name like "Classic Black"
+  frameDescription: { type: String, required: true }, // Description like "A timeless black frame."
+  frameWidth: { type: Number, required: true }, // Width of the frame
+  frameDepth: { type: Number, required: true }, // Depth of the frame
+  frameCategory: { type: String, required: true }, // Frame category like "Classic"
+  frameImage: { type: String, required: true }, // Image file name or URL
+  
+  sides: {
+    sideUp: { type: String, required: true }, // URL or path for side up image
+    sideDown: { type: String, required: true }, // URL or path for side down image
+    sideLeft: { type: String, required: true }, // URL or path for side left image
+    sideRight: { type: String, required: true }, // URL or path for side right image
+    topLeft: { type: String, required: true }, // URL or path for top left image
+    topRight: { type: String, required: true }, // URL or path for top right image
+    bottomLeft: { type: String, required: true }, // URL or path for bottom left image
+    bottomRight: { type: String, required: true }, // URL or path for bottom right image
+  },
 });
 
-module.exports = mongoose.model('FrameSchema', frameSchema);
-
+module.exports = mongoose.model("FrameSchema", frameSchema);
--- src/services/UploadFIle/uploadToS3.js
+++ src/services/UploadFIle/uploadToS3.js
@@ -1,7 +1,6 @@
-const fs = require('fs');
-const AWS = require('aws-sdk');
-require('dotenv').config();
-
+const fs = require("fs");
+const AWS = require("aws-sdk");
+require("dotenv").config();
 
 AWS.config.credentials = new AWS.Credentials({
   accessKeyId: process.env.ACCESS_KEY_ID,
@@ -10,79 +9,31 @@ AWS.config.credentials = new AWS.Credentials({
 
 AWS.config.region = process.env.REGION;
 
-
 const s3 = new AWS.S3();
 
+function uploadImageToS3(filePath, fileName, contentType) {
 
-function uploadImageToS3(filePath, fileName, bucketName) {
-  
-  const fileContent = fs.readFileSync(filePath, { encoding: 'binary' });
+  const fileContent = fs.readFileSync(filePath);
 
   const params = {
-    Bucket: bucketName,
+    Bucket: process.env.AWS_BUCKET_NAME,
     Key: fileName,
     Body: fileContent,
-    ContentType: 'image/jpeg',
-    ACL: 'public-read',
+    ContentType: contentType,
+    ACL: "public-read",
   };
 
   return new Promise((resolve, reject) => {
     s3.upload(params, (err, data) => {
       if (err) {
-        console.error('Error uploading file:', err);
+        console.error("Error uploading file:", err);
         reject(err);
       } else {
-        console.log('File uploaded successfully. URL:', data.Location);
+        console.log("File uploaded successfully. URL:", data.Location);
         resolve(data.Location);
       }
     });
   });
 }
 
-
 module.exports = uploadImageToS3;
-
-
-// const fs = require('fs');
-// const AWS = require('aws-sdk');
-// require('dotenv').config();
-
-// const accessKeyId = process.env.ACCESS_KEY_ID;
-// const secretAccessKey = process.env.SECRET_ACCESS_KEY;
-// const region = process.env.REGION;
-
-// AWS.config.update({
-//   accessKeyId,
-//   secretAccessKey,
-//   region
-// });
-
-// // Create S3 instance
-// const s3 = new AWS.S3();
-
-// // Function to upload image to S3 and get back URL
-// function uploadImageToS3(filePath, fileName, bucketName) {
-//   const fileContent = fs.readFileSync(filePath);
-
-//   // Setting up S3 upload parameters
-//   const params = {
-//     Bucket: bucketName,
-//     Key: fileName, // File name you want to save as in S3
-//     Body: fileContent
-//   };
-//   console.log("pppppapapappapap", params);
-//   // Uploading files to the bucket
-//   return new Promise((resolve, reject) => {
-//     s3.upload(params, (err, data) => {
-//       if (err) {
-//         console.error('Error uploading file:', err);
-//         reject(err);
-//       } else {
-//         console.log('File uploaded successfully. URL:', data.Location);
-//         resolve(data.Location);
-//       }
-//     });
-//   });
-// }
-
-// module.exports = uploadImageToS3;
